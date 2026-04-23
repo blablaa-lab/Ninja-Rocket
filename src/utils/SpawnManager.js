0bd483cf0be0f6ff763a5ef99be02ca460c9e841
Guillaume <blablaa.studio@gmail.com>
2026-04-23 17:28:27 +0200

    update : gestion de diffusion des assets

--- src/utils/SpawnManager.js
+++ src/utils/SpawnManager.js
@@ -7,19 +7,19 @@ export class SpawnManager {
     this.difficultyStep = 20000;
 
     this.objectTypes = [
-      { key: 'nr-camille',  points: 30, isMember: true, weight: 3 },
-      { key: 'nr-damien',   points: 30, isMember: true, weight: 3 },
-      { key: 'nr-guillaume',points: 30, isMember: true, weight: 3 },
-      { key: 'nr-jc',       points: 30, isMember: true, weight: 3 },
-      { key: 'nr-lolo',     points: 30, isMember: true, weight: 3 },
-      { key: 'nr-mathilde', points: 30, isMember: true, weight: 3 },
-      { key: 'nr-nina',     points: 30, isMember: true, weight: 3 },
-      { key: 'nr-romain',   points: 30, isMember: true, weight: 3 },
-      { key: 'nr-sandrine', points: 30, isMember: true, weight: 3 },
-      { key: 'nr-assets-1', points: 10, isMember: false, weight: 4 }, // clavier
-      { key: 'nr-assets-2', points: 5,  isMember: false, weight: 4 }, // câble HDMI
-      { key: 'nr-assets-3', points: 15, isMember: false, weight: 3 }, // laptop
-      { key: 'nr-assets-4', points: 10, isMember: false, weight: 4 }, // café
+      { key: 'nr-camille',  points: 30, isMember: true, weight: 1 },
+      { key: 'nr-damien',   points: 30, isMember: true, weight: 1 },
+      { key: 'nr-guillaume',points: 30, isMember: true, weight: 1 },
+      { key: 'nr-jc',       points: 30, isMember: true, weight: 1 },
+      { key: 'nr-lolo',     points: 30, isMember: true, weight: 1 },
+      { key: 'nr-mathilde', points: 30, isMember: true, weight: 1 },
+      { key: 'nr-nina',     points: 30, isMember: true, weight: 1 },
+      { key: 'nr-romain',   points: 30, isMember: true, weight: 1 },
+      { key: 'nr-sandrine', points: 30, isMember: true, weight: 1 },
+      { key: 'nr-assets-1', points: 10, isMember: false, weight: 5 }, // clavier
+      { key: 'nr-assets-2', points: 5,  isMember: false, weight: 5 }, // câble HDMI
+      { key: 'nr-assets-3', points: 15, isMember: false, weight: 5 }, // laptop
+      { key: 'nr-assets-4', points: 10, isMember: false, weight: 5 }, // café
     ];
 
     // Groupe physique avec collision interne
